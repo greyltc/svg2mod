e635b9b6c6fa569bc3b2f0a28f1c0ef2e265793e
rhn <gihu.rhn@porcupinefactory.org>
2018-11-23 19:47:25 +0000

    Fix circle transformation

--- svg2mod/svg/svg/svg.py
+++ svg2mod/svg/svg/svg.py
@@ -553,9 +553,10 @@ class Ellipse(Transformable):
         return (pmin, pmax)
 
     def transform(self, matrix):
-        self.center = self.matrix * self.center
-        self.rx = self.matrix.xlength(self.rx)
-        self.ry = self.matrix.ylength(self.ry)
+        matrix *= self.matrix
+        self.center = matrix * self.center
+        self.rx = matrix.xlength(self.rx)
+        self.ry = matrix.ylength(self.ry)
 
     def scale(self, ratio):
         self.center *= ratio
