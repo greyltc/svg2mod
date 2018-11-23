19928ad868777207cc4cbffc2c971616eb20b955
rhn <gihu.rhn@porcupinefactory.org>
2018-11-23 18:34:30 +0000

    Move point back to start on closing
    This will allow font-made paths to be processed properly if they have disjoint pieces or holes.

--- svg2mod/svg/svg/svg.py
+++ svg2mod/svg/svg/svg.py
@@ -401,7 +401,7 @@ class Path(Transformable):
             # Close Path
                 l = Segment(current_pt, start_pt)
                 self.items.append(l)
-
+                current_pt = start_pt
 
             elif command in 'LHV':
             # LineTo, Horizontal & Vertical line
